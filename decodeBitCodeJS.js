412dcc1bcae73f622d8f291a9207ed69e2cc92b4
J. Famous <joshua.famous@gmail.com>
2017-05-19 09:17:29 -0400

    Split javascript into seperate file

---
+++ decodeBitCodeJS.js
@@ -0,0 +1,37 @@
+function DecryptHex(pop)
+	{
+    	var answerIndex = 0;
+		var	value;
+		var	newString;
+		var	code;
+		var	i;
+    var finalString = "";
+    
+    for(var loopPop=0; loopPop<pop.length;loopPop++){
+   		
+      var s = pop[loopPop];
+      answerIndex = loopPop;
+
+      value = (answerIndex % 31) + 1;
+      newString = "";
+      for (i=0; i < s.length; i += 2) {
+        code = parseInt(s.substring(i, i + 2), 16);
+        newString += String.fromCharCode(code ^ value);
+      }
+		
+    	finalString = finalString + " " + newString;
+		}
+		document.getElementById("demo").innerHTML = finalString;
+    
+	}
+
+function BreakInput(){
+	var bigString = document.getElementById("ans1").value;
+	var inpArray = [];
+	while(bigString.indexOf("] = '") != -1){
+		var subStr = bigString.substring( bigString.indexOf("] = '") + 5 , bigString.indexOf("] = '") + 7 );
+		inpArray.push(subStr);
+		bigString = bigString.replace( bigString.substring( bigString.indexOf("] = '"), bigString.indexOf("] = '")+8 ) ,"");
+	}
+	DecryptHex(inpArray);
+}
