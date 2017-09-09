c48d4998b4638553ea88d65b772e8fcf9b1c8a24
J. Famous <joshua.famous@gmail.com>
2017-09-08 23:30:29 -0400

    Migrate Script into js file

--- decodeBitCodeJS.js
+++ decodeBitCodeJS.js
@@ -1,3 +1,24 @@
+//accordion tab function
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
+//actually decrypt the user input
 function decryptHex(pop){
     	var answerIndex = 0;
 		var	value;
@@ -24,6 +45,7 @@ function decryptHex(pop){
     
 }
 
+//break it down now y'all
 function breakInput(){
 	bigString = document.getElementById("ans1").value;
 
